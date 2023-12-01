89de1d5e61bcb8898c5bd060bca856cb618ddef0
Gregory Holder <gregory.holder76@gmail.com>
2023-12-01 10:50:45 +0100

    Add grid

--- tp3/tp3.m
+++ tp3/tp3.m
@@ -15,61 +15,52 @@ xmi = sin (2* pi * Fmi * t); % signal sonore pour la note   MI
 x_notes =[ xdo xre xmi ]; % concatenation des trois signaux
 t_notes = (0:length(x_notes) - 1) / Fe;
 
+figure;
+spec_grid(x_notes, 50:50:200, 0:0.2:0.8, Fe);
 
-% [s, f, t] = TF_glissante(x_notes, 50, 0, Fe);
-% 
-% figure();
-% mesh(t, f, abs(s'));
-% view(2), axis tight
+%% 
+Fe = 1e3; % frequence d ' echantillonnage
+t = 0:1/ Fe :2; % vecteur temps sur 2 s
+x_chirp_lin = chirp(t ,100 ,1 ,250) ; % chirp lineaire
 
+figure;
+spec_grid(x_chirp_lin, 50:50:200, 0:0.2:0.8, Fe);
 
-% %% 
-% Fe = 1e3; % frequence d ' echantillonnage
-% t = 0:1/ Fe :2; % vecteur temps sur 2 s
-% x_chirp_lin = chirp(t ,100 ,1 ,250) ; % chirp lineaire
-% 
-% figure();
-% 
-% [s, f, t] = TF_glissante(x_chirp_lin, 50, 25, Fe);
-% 
-% mesh(t, f, abs(s'));
-% view(2), axis tight
-% 
-% 
-% %% 
-% Fe = 1e3; % frequence d ' echantillonnage
-% t = 0:1/ Fe :2; % vecteur temps sur 2 s
-% x_chirp_quad = chirp(t ,100 ,1 ,250 , 'quadratic') ; % chirp quadratique
-% 
-% figure();
-% 
-% [s, f, t] = TF_glissante(x_chirp_quad, 50, 25, Fe);
-% 
-% mesh(t, f, abs(s'));
-% view(2), axis tight
+%% 
+Fe = 1e3; % frequence d ' echantillonnage
+t = 0:1/ Fe :2; % vecteur temps sur 2 s
+x_chirp_quad = chirp(t ,100 ,1 ,250 , 'quadratic') ; % chirp quadratique
 
-
-
-% %%
-% bird = load('bird.mat');
-% 
-% figure();
-% [s, f, t] = TF_glissante(bird.data', 100, 25, bird.fs);
-% 
-% mesh(t, f, abs(s'));
-% view(2), axis tight
-
+figure;
+spec_grid(x_chirp_quad, 50:50:200, 0:0.2:0.8, Fe);
 
 %%
+bird = load('bird.mat');
+
+figure;
+spec_grid(bird.data', 50:50:200, 0:0.2:0.8, bird.fs);
+
+%
 dolphin = load('gd_dauphin.mat');
 
-figure();
-[s, f, t] = TF_glissante(dolphin.y(:, 1)', 10000, 0, dolphin.fs);
-
-mesh(t, f, abs(s'));
-view(2), axis tight
+figure;
+spec_grid(dolphin.y(:, 1)', 200:400 :1000, 0:0.4:0.8, dolphin.fs);
 
 
+function spec_grid(x, widths, overlaps, Fs)
+    tiledlayout(length(widths), length(overlaps), 'Padding', 'none', 'TileSpacing', 'compact');
+    for width=widths
+        for o=overlaps
+            overlap = floor(o * width);
+            [s, f, t] = TF_glissante(x', width, overlap, Fs);
+            
+            nexttile;
+            image(t, f, abs(s), 'CDataMapping', 'scaled');
+            axis xy;
+            title(compose("%d - %2.f%%", width, o * 100));
+        end
+    end
+end
 
 
 
