ff53e45565f570c5936604b16a1a216ec9069861
Gregory Holder <gregory.holder76@gmail.com>
2023-12-01 10:50:32 +0100

    Only do fft on window

--- tp3/TF_glissante.m
+++ tp3/TF_glissante.m
@@ -3,28 +3,22 @@ function [s, f, t] = TF_glissante(x, width, overlap, Fs)
     len = length(x);
     step_size = width - overlap;
 
-    nb_steps = floor(len / step_size);
+    nb_steps = floor((len- width) / step_size);
 
-    H = hamming(width);
+    H = hamming(width)';
   
-    for step = 0:nb_steps-2
+    for step = 0:nb_steps-1
         offset = step * step_size;
-
-        w = rectpuls((1:len) - offset - width / 2, width);
-        
-        % w = zeros(1, len);
-
-        % w(1 + offset : offset + width) = H;
     
-        section = x .* w;
+        section = x(1 + offset: offset + width) .* H;
 
         F = fft(section);
         
-        s(step+1, :) = F(1:floor(len/2));
+        s(:, step+1) = F(1:floor(width/2));
     end
 
     t = (0:nb_steps-2) * step_size / Fs;
 
-    f = (0:floor(len/2) - 1) * Fs / len;
+    f = (0:floor(width/2) - 1) * Fs / width;
 
 end
